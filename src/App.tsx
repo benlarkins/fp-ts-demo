5a12b27933553133034dc2c9ccd087c357540d0a
Ben Larkins <benlarkins@pm.me>
2025-02-19 18:23:27 -0700

    Remove counter component. Make login component root

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Navigate, Routes, Route } from 'react-router-dom';
 
 import { store } from '@/store/store';
-import { Counter } from '@/components/Counter';
 import { Login } from '@/components/Login';
 import { Provider as ChakraProvider } from '@/components/ui/provider';
 
@@ -13,8 +12,7 @@ function App() {
       <ChakraProvider>
         <Router>
           <Routes>
-            <Route path="/" element={<Counter />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </Router>
